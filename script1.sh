dd9adadee1598603f08cc0f72b2fc67bc97e03ff
x1rro <artempechischev@mail.ru>
2025-03-06 20:13:07 +0000

    upgrade script1

--- script1.sh
+++ script1.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "Hello"
-ls
-echo "world"
+mycomputer="honor"
+myOS=`uname -a`
 
-ifconfig
+echo "Name script is $0"
+echo "Privet $1"
+echo "Hello $2"
 
-echo "done"
+num1=50
+num2=45
+summa=$((num1+num2))
+echo "$num1 + $num2 = $summa"
