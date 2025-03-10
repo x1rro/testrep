fce50045279b260c9a4514bd77c97ce1d5f45f39
artem <pechischev@gmail.com>
2025-03-10 11:00:46 +0300

    hueta

--- script1.sh
+++ script1.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-a1=Hello
-a2="hello"
-
-if [ $a1 \> $a2 ]
-then
-echo "a1 greater then a2"
-else
-echo "a1 is less then a2"
-fi
+for((i = 0;i < 10; i++))
+do
+echo "Number is $i"
+done
+echo (("5"+"5"))
